c710db0f2d7cbb99c32530113025726af37bb9c4
zakisserious <147680120+zakisserious@users.noreply.github.com>
2025-02-13 09:03:09 +0800

    Formatting Changes

--- src/HRClient.java
+++ src/HRClient.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.hr_system;
 
-/**
- *
- * @author zakaria
- */
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
