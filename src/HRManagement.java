1793cb88f906830b8f2b9ec58e883913874be5a5
zakisserious <147680120+zakisserious@users.noreply.github.com>
2025-02-15 09:20:39 +0800

    Update HRManagement.java

--- src/HRManagement.java
+++ src/HRManagement.java
@@ -1,17 +1,16 @@
 package com.mycompany.hr_system;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 public interface HRManagement extends Remote {
-    boolean addEmployee(String firstName, String lastName, String icPassport, String role, String department) throws RemoteException;
+    boolean addEmployee(String icPassport, String firstName, String lastName, String role, String department)
+            throws RemoteException;
+
     List<String> viewEmployees() throws RemoteException;
+
     boolean updateEmployeeRole(String icPassport, String newRole) throws RemoteException;
-    String generateYearlyLeaveReport(String icPassport) throws RemoteException;
-    boolean approveLeave(String icPassport, String leaveId) throws RemoteException;
-    boolean rejectLeave(String icPassport, String leaveId) throws RemoteException;
-    boolean deleteEmployee(String icPassport) throws RemoteException; 
-    List<String> viewLeaveApplications() throws RemoteException; 
-    boolean applyForLeave(String icPassport, String startDate, String endDate, String reason) throws RemoteException; 
-    
+
+    boolean deleteEmployee(String icPassport) throws RemoteException;
 }
