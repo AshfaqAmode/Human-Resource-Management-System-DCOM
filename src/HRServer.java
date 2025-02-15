10f012adbbe671263ac8058869c6e49b4a20d115
zakisserious <147680120+zakisserious@users.noreply.github.com>
2025-02-15 09:21:40 +0800

    Bug fixes on HRServer.java

--- src/HRServer.java
+++ src/HRServer.java
@@ -1,20 +1,26 @@
-
 package com.mycompany.hr_system;
 
-
-import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class HRServer {
     public static void main(String[] args) {
         try {
-            HRManagementImpl system = new HRManagementImpl();
-            Naming.rebind("rmi://localhost/HRManagement", system);
-            System.out.println("HR Management System is ready.");
+            System.setProperty("java.rmi.server.hostname", "127.0.0.1");
+
+            // Create the RMI registry on port 1098
+            Registry registry = LocateRegistry.createRegistry(1098);
+
+            // Create the remote object
+            HRManagementImpl hrSystem = new HRManagementImpl();
+
+            // Bind the remote object to the registry
+            registry.rebind("HRSystem", hrSystem);
+
+            System.out.println("HR Server is up and running...");
         } catch (Exception e) {
             System.err.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
-
